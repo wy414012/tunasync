1395f413b80042d134ab790223cbc9c285518086
Yaming <ymwlpoolc@qq.com>
2022-01-31 11:42:22 +0800

    添加多系统构建脚本

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e # Exits on error
+
+NAME=$(date "+%Y_%m_%d")
+OSID=$(uname)
+
+if [ "$OSID" = "Darwin" ]; then
+{
+for i in linux-amd64 linux-arm64; do
+    make ARCH=$i all
+    done
+mkdir -p build
+cd build
+zip -qr -FS ./"tunasync-$NAME-linux-amd64.zip" ../build-linux-amd64/*
+zip -qr -FS ./"tunasync-$NAME-linux-arm64.zip" ../build-linux-arm64/*
+}
+else
+if [ "$OSID" = "Linux" ]; then
+{
+for i in linux-amd64 linux-arm64; do
+    make ARCH=$i all
+    done
+    mkdir -p build
+    cd build
+zip -qr -FS ./"tunasync-$NAME-linux-amd64.zip" ../build-linux-amd64/*
+zip -qr -FS ./"tunasync-$NAME-linux-arm64.zip" ../build-linux-arm64/*
+    }
+fi
+fi
+
+exit 0
